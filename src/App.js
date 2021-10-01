ec2c470ff6ebf482e93a8928a8ada43ebc38c062
React_Teaching_Project <priyassss789@gmail.com>
2021-10-01 17:01:45 +0530

    Adding re-usable card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import AddUser from './component/User/AddUser';
 
 
 function App() {
   return (
     <div>
-
+      <AddUser/>
     </div>
   );
 }
 
 export default App;
+
+
