f9d3b6161c7a2b3855e801aa6f60f9eb4eb3c0f3
React_Teaching_Project <priyassss789@gmail.com>
2021-10-05 12:35:12 +0530

    Managing  error starte

--- src/component/User/AddUser.js
+++ src/component/User/AddUser.js
@@ -9,14 +9,23 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     /* trim will remove white space */
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: " Please enter a valid name & age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: " Please enter a valid age (> 0).",
+      });
       return;
     }
 
@@ -32,9 +41,19 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           {/* htmlFor = assigning props to html attributes */}
